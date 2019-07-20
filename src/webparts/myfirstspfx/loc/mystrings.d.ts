7bdd5c29bcf09da9709b1aa38dc2f7deca98d728
rahulsnisongithub <rahulsn@hotmail.com>
2019-07-20 15:03:57 +0530

    initial commit fixes AB#3

---
+++ src/webparts/myfirstspfx/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMyfirstspfxWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MyfirstspfxWebPartStrings' {
+  const strings: IMyfirstspfxWebPartStrings;
+  export = strings;
+}
